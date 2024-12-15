1af66c2707558c1368df65f0599dc773132ba555
kumar swamy <yarramneedikumarswamy@gmail.com>
2024-12-15 17:14:08 +0530

    signup route is setuped

--- backend/routes/UserRoute/user.js
+++ backend/routes/UserRoute/user.js
@@ -1,7 +1,12 @@
-const express=require('express')
+const express=require('express');
+const userSignupController = require('../../controllers/userControllers/userSignupController');
+
 
 var routes=express.Router();
 
+// user route paths
+
+routes.post('/signup',userSignupController)
 
 
 module.exports=routes;
