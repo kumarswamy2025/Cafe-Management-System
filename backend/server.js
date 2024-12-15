8d2d64a00514ca2fb2e072e8a7a94d5072276abe
kumar swamy <yarramneedikumarswamy@gmail.com>
2024-12-15 10:05:12 +0530

    server is completely setuped'

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,24 @@
+// configuring dotenv here because  we using here 
 require('dotenv').config();
+
+// importing http module to create server
 const http=require('http');
+
+// importing backend application root  file 
 const app=require('./index');
 
-
+// creating server and passing the backend application root  file 
 const server=http.createServer(app);
 
-server.listen(process.env.PORT);
+
+// starting server with help of listen() method 
+server.listen(process.env.PORT,(error)=>{
+    if(!error){
+        console.log("SERVER IS SUCCESSFULLY STARTED...... AT PORT :",process.env.PORT);
+        
+    }
+    else{
+        console.log("SERVER  ERROR ",error);
+        
+    }
+});
