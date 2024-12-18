58cc3aa2fc9d05e872670442bf3f844081d017b0
kumar swamy <yarramneedikumarswamy@gmail.com>
2024-12-18 16:03:35 +0530

    adding user  login controller in  route

--- backend/routes/UserRoute/user.js
+++ backend/routes/UserRoute/user.js
@@ -1,14 +1,21 @@
 const express=require('express');
 const userSignupController = require('../../controllers/userControllers/userSignupController');
+const UserSignInController = require('../../controllers/userControllers/userSigninController');
+const passwordResetController = require('../../controllers/userControllers/passwardResetController');
 
 
 var routes=express.Router();
 
 // user route paths
-
+// signup  route
 routes.post('/signup',userSignupController)
 
+// signin route
 
+routes.post('/login',UserSignInController)
 
+// password reset route
+
+routes.post('/passwordreset',passwordResetController);
 
 module.exports=routes;
