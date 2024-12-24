bb577b8120a66a53ca00f59e635437bf3a32843a
kumar swamy <yarramneedikumarswamy@gmail.com>
2024-12-24 10:13:29 +0530

    implementing  checking role middleware

---
+++ backend/services/checkRole.js
@@ -0,0 +1,27 @@
+async function checkRole(req, res, next) {
+    try {
+        if (req.locals.role == process.env.USER) {
+
+            return res.status(400).json({
+                message:"you dont have a permission to access this content",
+                success:false,
+                error:true
+            })
+
+        }
+        else{
+            next();
+        }
+
+    } catch (error) {
+
+        return res.status(400).json({
+            message:"there is an error in check role file ",
+            success:false,
+            error:true
+        })
+
+    }
+}
+
+module.exports = checkRole
