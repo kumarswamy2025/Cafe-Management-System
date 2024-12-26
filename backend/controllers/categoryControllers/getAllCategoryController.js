1ad75fa698fa779ca4fd7e1f8c1efb249ec16153
kumar swamy <yarramneedikumarswamy@gmail.com>
2024-12-26 15:29:59 +0530

    get all category controller is setuped sucessfully

---
+++ backend/controllers/categoryControllers/getAllCategoryController.js
@@ -0,0 +1,43 @@
+// importing data base
+const connectDB = require('../../connectDB');
+
+async function getAllCategoryController(req,res) {
+
+    try {
+
+        const getAllCategoryControllerQuery="select *  from  category  order by name "
+
+        connectDB.query(getAllCategoryControllerQuery,(getAllCategoryControllerQueryError,getAllCategoryControllerQueryResult)=>{
+
+
+
+            if(!getAllCategoryControllerQueryError){
+
+                return res.status(400).json({
+                    message:"all categories fetched success fully ",
+                    success:true,
+                    error:false,
+                    SuccessData:getAllCategoryControllerQueryResult
+                })
+
+            }
+            else{
+                return res.status(400).json({
+                    message:"there is an error in getAllCategoryControllerQuery",
+                    success:false,
+                    error:true,
+                    errorData:getAllCategoryControllerQueryError
+                })
+
+            }
+
+        })
+
+
+    } catch (error) {
+        
+    }
+    
+}
+
+module.exports=getAllCategoryController;
