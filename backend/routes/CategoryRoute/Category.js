5f6c84bcc154622c21e5f93214816e377f1a493f
kumar swamy <yarramneedikumarswamy@gmail.com>
2024-12-26 15:30:41 +0530

    get all category route is setuped

--- backend/routes/CategoryRoute/Category.js
+++ backend/routes/CategoryRoute/Category.js
@@ -3,13 +3,19 @@ const express=require('express')
 const addCategoryController = require('../../controllers/categoryControllers/addCategoryController')
 // importing router method avalable in express module 
 const routes=express.Router()
+// importing check role 
+const checkRole= require('../../services/checkRole')
+
 
 const authenticateToken = require('../../services/authenticateToken');
+const getAllCategoryController = require('../../controllers/categoryControllers/getAllCategoryController');
 
 
 // add category route 
-routes.post('/addCategory',authenticateToken,addCategoryController)
+routes.post('/addCategory',authenticateToken,checkRole,addCategoryController)
 
+// getting all categories 
+routes.get('/getAllCategory',authenticateToken,getAllCategoryController)
 
 
 module.exports=routes
