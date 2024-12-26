80df087cd330761c3049b8c055909b7e79bf95e2
kumar swamy <yarramneedikumarswamy@gmail.com>
2024-12-26 15:04:14 +0530

    category route is setuped suceessfully

--- backend/routes/CategoryRoute/Category.js
+++ backend/routes/CategoryRoute/Category.js
@@ -1,8 +1,15 @@
 // importing express module 
 const express=require('express')
+const addCategoryController = require('../../controllers/categoryControllers/addCategoryController')
 // importing router method avalable in express module 
-const route=express.Router()
+const routes=express.Router()
+
+const authenticateToken = require('../../services/authenticateToken');
+
+
+// add category route 
+routes.post('/addCategory',authenticateToken,addCategoryController)
 
 
 
-
+module.exports=routes
